9a6adc77a16a62da1cd5d82e0d78ac1693eea079
Steve D <steved@localhost.localdomain>
2019-10-17 17:06:59 -0400

    ...

--- splendor/core.py
+++ splendor/core.py
@@ -1,6 +1,9 @@
+"""
+"""
+
 from enum import Enum
 
-class GemTypeEnum(Enum)
+class GemTypeEnum(Enum):
     NONE = 0
     BLACK = 1
     BLUE = 2
@@ -9,118 +12,163 @@ class GemTypeEnum(Enum)
     WHITE = 5
     YELLOW = 10
 
-class GemType(GemTypeEnum)
-    __init__(enum_val: int)
-    __str__() -> str # get_desc()
-    get_desc() -> str # e.g. "yellow"
-    get_desc_long() -> str # e.g. "gold"
+GEM_TYPE_STR_DICT = {
+        GemTypeEnum.NONE: ("none", "none"),
+        GemTypeEnum.BLACK: ("black", "onyx"),
+        GemTypeEnum.BLUE: ("blue", "sapphire"),
+        GemTypeEnum.GREEN: ("green", "emerald"),
+        GemTypeEnum.RED: ("red", "ruby"),
+        GemTypeEnum.WHITE: ("white", "diamond"),
+        GemTypeEnum.YELLOW: ("yellow", "gold")
+        }
 
-class DevCardType(GemType)
+class GemType:
+    """
+    >>> a = GemType(GemTypeEnum.BLACK)
+    >>> a.get_desc()
+    'black'
+    >>> a.get_desc_long()
+    'black (onyx)'
+    """
+
+    t: GemTypeEnum
+    
+    def __init__(
+            self,
+            enum_val: GemTypeEnum,
+            ) -> None:
+        self.t = enum_val
+    
+    def __str__(self) -> str:
+        return self.get_desc()
+    
+    def get_desc(self) -> str:
+        return f"{GEM_TYPE_STR_DICT[self.t][0]}"
+    
+    def get_desc_long(self) -> str:
+        return f"{GEM_TYPE_STR_DICT[self.t][0]} " + \
+                f"({GEM_TYPE_STR_DICT[self.t][1]})"
+
+
+class DevCardType(GemType):
+    """
+    >>> a = DevCardType(GemTypeEnum.BLACK)
+    >>> a.get_desc()
+    'black'
+    >>> a.get_desc_long()
+    'black (onyx)'
+    """
+    pass
 
 class DevCard:
+    """
+    >>> a = DevCard(level=1, t=
     level: int # 1, 2, or 3
     type: DevCardType # same as bonus
-    ppoints: int
-    cost: dict # GemType (or TokenType?) -> count
-    __int__(level: int, type: DevCardType, ppoints: int, cost: dict)
-    is_purchasable(dev_card_cache: DevCardCache, token_cache: TokenCache) -> bool
-    __str__() -> str
-    get_str() -> str # __str__()
-    get_image() -> bytes
+#    ppoints: int
+#    cost: dict # GemType (or TokenType?) -> count
+#    __int__(level: int, type: DevCardType, ppoints: int, cost: dict)
+#    is_purchasable(dev_card_cache: DevCardCache, token_cache: TokenCache) -> bool
+#    __str__() -> str
+#    get_str() -> str # __str__()
+#    get_image() -> bytes
 
-class DevCardCache:
-    d: dict # DevCardType -> set(DevCard)
-    __init__() # create empty Cache
-    add(dev_card: DevCard)
-    remove(dev_card: DevCard) -> None # remove dev_card matching some DevCard in the Cache, or raise exception.  For undoing.                                                    
-    calc_ppoints() -> int # calc ppoints across this cache
-    calc_discount(dev_card_type: DevCardType) -> int # return current discount for DevCardType arg                                                                               
-    __str__() -> str
-    get_str() -> str # __str__()
-    get_image() -> bytes
+#class DevCardCache:
+#    d: dict # DevCardType -> set(DevCard)
+#    __init__() # create empty Cache
+#    add(dev_card: DevCard)
+#    remove(dev_card: DevCard) -> None # remove dev_card matching some DevCard in the Cache, or raise exception.  For undoing.                                                    
+#    calc_ppoints() -> int # calc ppoints across this cache
+#    calc_discount(dev_card_type: DevCardType) -> int # return current discount for DevCardType arg                                                                               
+#    __str__() -> str
+#    get_str() -> str # __str__()
+#    get_image() -> bytes
 
-DEV_CARD_RESERVE_COUNT_MAX = 3
+#DEV_CARD_RESERVE_COUNT_MAX = 3
 
-class DevCardReserve:
-    s: set
-    __init__()
-    add(dev_card: DevCard)
-    remove(dev_card: DevCard) -> None # remove dev_card matching some DevCard in the Cache, or raise exception.  For undoing.
-    is_max() -> bool
-    can_action_reserve() -> bool
-    __str__() -> str
-    get_str() -> str # __str__()
+#class DevCardReserve:
+#    s: set
+#    __init__()
+#    add(dev_card: DevCard)
+#    remove(dev_card: DevCard) -> None # remove dev_card matching some DevCard in the Cache, or raise exception.  For undoing.
+#    is_max() -> bool
+#    can_action_reserve() -> bool
+#    __str__() -> str
+#    get_str() -> str # __str__()
 
-class DevCardDeck: # a set of all the dev cards of one level; for storing Game decks
-    level: int
-    l: list # indices 0..3 are the face-up cards; 4..n are the face-down, where 4 is the top-most
-    __init__(level: int, s: set) # load all of this level's cards
-    get_level() -> int
-    get_list() -> List
-    get_facing() -> List[4] # return cards at indices 0..3, which are the facing ones
-    shuffle() -> None
-    pop_by_idx(idx: int) -> DevCard # remove DevCard at index idx and return it, or raise exc if oob
-    pop_hidden_card() -> DevCard # remove DevCard at index 4, which is the top of the deck, and return it
-    is_empty()
-    count()
-    __str__() -> str
-    get_image() -> bytes
+#class DevCardDeck: # a set of all the dev cards of one level; for storing Game decks
+#    level: int
+#    l: list # indices 0..3 are the face-up cards; 4..n are the face-down, where 4 is the top-most
+#    __init__(level: int, s: set) # load all of this level's cards
+#    get_level() -> int
+#    get_list() -> List
+#    get_facing() -> List[4] # return cards at indices 0..3, which are the facing ones
+#    shuffle() -> None
+#    pop_by_idx(idx: int) -> DevCard # remove DevCard at index idx and return it, or raise exc if oob
+#    pop_hidden_card() -> DevCard # remove DevCard at index 4, which is the top of the deck, and return it
+#    is_empty()
+#    count()
+#    __str__() -> str
+#    get_image() -> bytes
 
-class DevCardDecks:
-    l: list # indices 0..2 are decks #1..3
+#class DevCardDecks:
+#    l: list # indices 0..2 are decks #1..3
 
-DEV_CARD_DECKS = [set(), set(), set()] # to fill in with actual cards
+#DEV_CARD_DECKS = [set(), set(), set()] # to fill in with actual cards
 
-class Noble:
-    ppoints: int
-    cost: dict # GemType (or DevCardType?) -> count
-    image: bytes
-    __init__(ppoints: int, cost: dict, image=None: bytes)
-    __str__() -> str
-    get_image() -> bytes
+#class Noble:
+#    ppoints: int
+#    cost: dict # GemType (or DevCardType?) -> count
+#    image: bytes
+#    __init__(ppoints: int, cost: dict, image=None: bytes)
+#    __str__() -> str
+#    get_image() -> bytes
 
-class NoblesInPlay:
-    s: Set[Noble]
-    __init__(players_count: int)
-    __str__() -> str
+#class NoblesInPlay:
+#    s: Set[Noble]
+#    __init__(players_count: int)
+#    __str__() -> str
 
-NOBLES_DECK = set(...) # to fill in with actual noble cards
+#NOBLES_DECK = set(...) # to fill in with actual noble cards
 
-class TokenType(GemType)
+#class TokenType(GemType)
 
-class Token:
-    type: TokenType
-    desc: str
-    desc_long: str
-    image: bytes
-    __init__(token_type=TokenType.NONE: TokenType)
-    set_type_by_str(type_str)
-    __str__() # "sapphire (blue)"
-    get_image() -> bytes
+#class Token:
+#    type: TokenType
+#    desc: str
+#    desc_long: str
+#    image: bytes
+#    __init__(token_type=TokenType.NONE: TokenType)
+#    set_type_by_str(type_str)
+#    __str__() # "sapphire (blue)"
+#    get_image() -> bytes
 
-class TokenCache:
-    d: dict # TokenType -> count
-    __init__()
-    fill(players_count: int) -> None # for use by __init__()
-    empty() -> None
-    add(token_type: TokenType) -> None
-    remove(token_type: TokenType) -> None
-    count() -> int
-    count_type(token_type: TokenType) -> int
-    is_type_empty(token_type: TokenType) -> bool
-    can_action_take_three_gems(gem_types_set: Set(3)) -> bool
-    can_action_take_two_gems(gem_type: GemType) -> bool
+#class TokenCache:
+#    d: dict # TokenType -> count
+#    __init__()
+#    fill(players_count: int) -> None # for use by __init__()
+#    empty() -> None
+#    add(token_type: TokenType) -> None
+#    remove(token_type: TokenType) -> None
+#    count() -> int
+#    count_type(token_type: TokenType) -> int
+#    is_type_empty(token_type: TokenType) -> bool
+#    can_action_take_three_gems(gem_types_set: Set(3)) -> bool
+#    can_action_take_two_gems(gem_type: GemType) -> bool
 
-TOKEN_PLAYER_CACHE_MAX = 10
+#TOKEN_PLAYER_CACHE_MAX = 10
 
-class TokenPlayerCache(TokenCache)
-    __init__()
-    is_max() -> bool
-    is_over_max() -> bool
+#class TokenPlayerCache(TokenCache)
+#    __init__()
+#    is_max() -> bool
+#    is_over_max() -> bool
 
 # players count -> tokens count per type
-TOKEN_COUNT_MAP = {2: 4, 3: 6, 4: 7}
+#TOKEN_COUNT_MAP = {2: 4, 3: 6, 4: 7}
 
-class TokenGameCache(TokenCache)
-    __init__(players_count: int)
+#class TokenGameCache(TokenCache)
+#    __init__(players_count: int)
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
