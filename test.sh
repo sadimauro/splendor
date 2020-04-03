01c424fffb299dc25e3a633996b65f04b8d2b2d2
Steve D <steved@localhost.localdomain>
2020-04-03 10:31:32 -0400

    Adde arg parsing and functions to test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,51 @@
-python3 -m doctest splendor/core.py
-#python3 -m doctest splendor/game.py
-python3 -m doctest splendor/player.py
+#!/bin/bash
+
+VERBOSE=0
+
+# 
+# arg parsing
+# from https://medium.com/@Drew_Stokes/bash-argument-parsing-54f3b81a6a8f
+#
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    -v|--verbose)
+      VERBOSE=1
+      shift 1
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
+
+if [ $VERBOSE -eq 1 ]
+then
+	VERBOSE_FLAG="-v"
+else
+	VERBOSE_FLAG=""
+fi
+
+# args: module_dir
+function doctest_module () {
+	python3 -m doctest $1 $VERBOSE_FLAG
+}
+
+#
+# start testing
+#
+doctest_module splendor/core.py 
+#doctest_module splendor/game.py 
+doctest_module splendor/player.py 
 
