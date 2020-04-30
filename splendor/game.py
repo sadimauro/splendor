7c153b826e93e0569080fdebe45cf14e6f2d28a4
Steve D <steved@localhost.localdomain>
2020-04-29 21:54:25 -0400

    some game.py additions

--- splendor/game.py
+++ splendor/game.py
@@ -8,6 +8,50 @@ PLAYERS_COUNT_MAX = 4
 class GameState:
     """
     Record of a particular state of the game.
+
+    >>> dc0 = DevCard(level=1, t=DevCardType("black"), ppoints=2, cost={"blue": 2, "red": 1})
+    >>> dc1 = DevCard(level=1, t=DevCardType("black"), ppoints=0, cost={"blue": 3})
+    >>> dc2 = DevCard(level=1, t=DevCardType("blue"), ppoints=1, cost={"white": 1, "red": 1, "green": 3})
+    >>> dc3 = DevCard(level=1, t=DevCardType("red"), ppoints=4, cost={"white": 1, "red": 1, "green": 3})
+    >>> dc4 = DevCard(level=1, t=DevCardType("red"), ppoints=4, cost={"white": 1, "red": 1, "green": 3})
+    >>> dc5 = DevCard(level=1, t=DevCardType("red"), ppoints=0, cost={"red": 1, "green": 3})
+    >>> dc6 = DevCard(level=1, t=DevCardType("white"), ppoints=0, cost={"white": 1, "green": 3})
+    >>> dev_card_deck_1 = DevCardDeck(1, [dc0, dc1, dc2, dc3, dc4, dc5, dc6])
+
+    >>> dc0 = DevCard(level=2, t=DevCardType("black"), ppoints=2, cost={"blue": 2, "red": 1})
+    >>> dc1 = DevCard(level=2, t=DevCardType("black"), ppoints=0, cost={"blue": 3})
+    >>> dc2 = DevCard(level=2, t=DevCardType("blue"), ppoints=1, cost={"white": 1, "red": 1, "green": 3})
+    >>> dc3 = DevCard(level=2, t=DevCardType("red"), ppoints=4, cost={"white": 1, "red": 1, "green": 3})
+    >>> dc4 = DevCard(level=2, t=DevCardType("red"), ppoints=4, cost={"white": 1, "red": 1, "green": 3})
+    >>> dev_card_deck_2 = DevCardDeck(2, [dc0, dc1, dc2, dc3, dc4])
+    
+    >>> dc0 = DevCard(level=3, t=DevCardType("black"), ppoints=4, cost={"blue": 2, "red": 5})
+    >>> dc1 = DevCard(level=3, t=DevCardType("black"), ppoints=5, cost={"blue": 6})
+    >>> dc2 = DevCard(level=3, t=DevCardType("blue"), ppoints=5, cost={"white": 3, "red": 5, "green": 3})
+    >>> dc3 = DevCard(level=3, t=DevCardType("red"), ppoints=4, cost={"white": 3, "red": 3, "green": 3})
+    >>> dc4 = DevCard(level=3, t=DevCardType("red"), ppoints=4, cost={"white": 3, "red": 3, "green": 3})
+    >>> dc5 = DevCard(level=3, t=DevCardType("red"), ppoints=5, cost={"red": 4, "green": 3})
+    >>> dc6 = DevCard(level=3, t=DevCardType("white"), ppoints=4, cost={"white": 5, "green": 3})
+    >>> dev_card_deck_3 = DevCardDeck(3, [dc0, dc1, dc2, dc3, dc4, dc5, dc6])
+
+    >>> n1 = Noble(3, {'black': 4, 'white': 4})
+    >>> n2 = Noble(3, {'black': 3, 'white': 3, 'blue': 3})
+    >>> n3 = Noble(3, {'black': 4, 'green': 4})
+    >>> n4 = Noble(3, {'white': 4, 'red': 4})
+    >>> nobles_in_play = NoblesInPlay(set((n1, n2, n3, n4)))
+
+    >>> game_token_cache = GameTokenCache(players_count=2)
+    
+    >>> game_state = GameState(
+            dev_card_deck_1,
+            dev_card_deck_2,
+            dev_card_deck_3,
+            nobles_in_play,
+            game_token_cache,
+            )
+
+
+
     """
     dev_card_deck_1: DevCardDeck
     dev_card_deck_2: DevCardDeck
@@ -37,6 +81,10 @@ class GameState:
 class GameStateHistory:
     """
     Record of all of the historical states of the game.
+
+    >>> game_state_history = GameStateHistory()
+    >>> game_state_history.append(game_state)
+
     """
     l: List[GameState]
     
@@ -44,7 +92,8 @@ class GameStateHistory:
         self.l = list()
 
     # __str__() # needed?
-      def append(self, new_state) -> None:
+    
+    def append(self, new_state) -> None:
         self.l.append(new_state)
         return
 
